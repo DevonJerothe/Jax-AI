6d757ccd56d9d1c7a073f0c420363f6680f8536e
Devon Jerothe <devonjerothe@gmail.com>
2026-05-04 14:12:19 -0400

    add chat alias for outer table

--- PocketAI/DB/CharacterRepository.swift
+++ PocketAI/DB/CharacterRepository.swift
@@ -112,16 +112,18 @@ class CharacterRepository: Repository {
 
             // Check for any chats associated with and only with this character. 
             // we want to delete these as well. The join record cascade will not delete them.
+            let chatAlias = TableAlias()
             let chatsWithOtherAssociations = ChatCharacterJoinRecord
                 .filter(Column("characterCardId") != item.id.uuidString)
-                .filter(Column("chatId") == Column("id"))
+                .filter(Column("chatId") == chatAlias[Column("id")])
                 .exists()
 
             let chatsToDelete = try ChatRecord
+                .aliased(chatAlias)
                 .filter(
                     ChatCharacterJoinRecord
                         .filter(Column("characterCardId") == item.id.uuidString)
-                        .filter(Column("chatId") == Column("id"))
+                        .filter(Column("chatId") == chatAlias[Column("id")])
                         .exists()
                 )
                 .filter(!chatsWithOtherAssociations)
